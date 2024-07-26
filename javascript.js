0431daed05cdaa677170e4d9dd35589846c343a6
SomjaniNileshMulchand <somjaninilesh@gmail.com>
2024-07-26 21:36:41 +0530

    HTML CSS JAVASCRIPT

---
+++ javascript.js
@@ -0,0 +1,79 @@
+// const color = document.querySelector("#one")
+const color = document.querySelector("#one")
+
+color.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "blue";
+})
+
+const doc = document.querySelector("#two")
+
+doc.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "Brown"
+})
+
+const change = document.querySelector("#three")
+
+change.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "black";
+})
+
+const one = document.querySelector("#four")
+
+one.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "red";
+})
+
+const two = document.querySelector("#five")
+
+two.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "yellow"
+})
+
+const three = document.querySelector("#six")
+
+three.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "green"
+})
+
+const four = document.querySelector("#seven")
+
+four.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "white"
+})
+
+const five = document.querySelector("#eight")
+
+five.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "gray"
+})
+
+const six = document.querySelector("#nine")
+
+six.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "pink"
+})
+
+const seven = document.querySelector("#ten")
+
+seven.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "purple"
+})
+
+const eight = document.querySelector("#eleven")
+
+eight.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "skyblue"
+})
+
+const nine = document.querySelector("#twelve")
+
+nine.addEventListener("click",()=>{
+    document.body.style.backgroundColor = "orange"
+})
+
+
+
+// const change = document.querySelector("color")
+// function color()  {
+//     document.body.style.backgroundColor = "red";
+// }
